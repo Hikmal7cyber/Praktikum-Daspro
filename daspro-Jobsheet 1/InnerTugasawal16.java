c018484027427cfa7b58517b6fc38d5f41f01542
Muhammad Hikmal Al-Ghifary <mhikmal.ag@gmail.com>
2025-03-27 14:11:34 +0700

    Jobsheet 1

---
+++ daspro-Jobsheet 1/InnerTugasawal16.java
@@ -0,0 +1,20 @@
+/**
+ * InnerTugasawal16
+ */
+public class InnerTugasawal16 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Muhammad Hikmal Al-Ghifary");
+        System.out.println("NIM\t\t: 244107020074");
+        System.out.println("Kelas\t\t: 1B");
+        System.out.println("No absen\t: 16");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: Bogor");
+        System.out.println("No HP\t\t: 082124494566");
+        System.out.println("Hobi\t\t: Badminton");
+    }
+}
+
+
